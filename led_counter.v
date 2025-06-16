822399906e3966ac685df506c69b0bd8ae19ec09
Ubiratan Tavares <ust1973@gmail.com>
2025-06-16 13:22:51 -0300

    implementação do módulo Counter

--- led_counter.v
+++ led_counter.v
@@ -3,8 +3,29 @@ module Counter #(
 ) (
     input  wire clk,
     input  wire rst_n,
-    output wire [7:0] leds
+	output wire [7:0] leds
 );
 
+	localparam ONE_SECOND = CLK_FREQ; // número de ciclos de clock em 1 segundo
+
+	reg [31:0] counter;
+
+	reg [7:0] leds_reg;
+
+	always @(posedge clk, negedge rst_n) begin
+		if (!rst_n) begin
+			counter <= 32'h0;
+			leds_reg <= 8'd0;	
+		end else begin
+			if (counter >= (ONE_SECOND - 1)) begin
+				counter <= 32'h0;
+				leds_reg <= leds_reg + 1;
+			end else begin
+				counter <= counter + 1;
+			end
+		end
+	end
+
+	assign leds = leds_reg;
 
 endmodule
